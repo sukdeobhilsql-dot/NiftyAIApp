7a25ceb51420272212f17ecca05fcbd3c14f563c
sukdeobhilsql-dot <sukdeobhilsql@gmail.com>
2026-07-12 15:27:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,8 @@ import {
   RefreshControl, Alert, Animated
 } from "react-native";
 
-const SERVER = "http://92.4.91.185:8080";   // ← apna VPS IP
+const SERVER = "https://92.4.91.185";
+
 
 // ── Color tokens ─────────────────────────────────────────────────
 const C = {
